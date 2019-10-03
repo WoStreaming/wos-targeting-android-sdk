368c0886deecc85121845cb35dcc4e974d12fc93
David Wright <dwright@wideorbit.com>
2019-10-03 12:18:11 -0700

    Remove androidx dependency

--- app/src/main/java/com/wideorbit/wostargetingandroidsdk/MainActivity.java
+++ app/src/main/java/com/wideorbit/wostargetingandroidsdk/MainActivity.java
@@ -1,22 +1,21 @@
 package com.wideorbit.wostargetingandroidsdk;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
 
 import com.wideorbit.wostreaming.WOSObserver;
 import com.wideorbit.wostreaming.WOSTargeting;
 
-public class MainActivity extends AppCompatActivity implements WOSObserver {
+public class MainActivity extends Activity implements WOSObserver {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		WOSTargeting WOSDK = new WOSTargeting(getApplicationContext(), 0, true, this);
+		WOSTargeting WOSDK = new WOSTargeting(getApplicationContext(), 6394, true, this);
 		WOSTargeting.enableDebug(false);
 		WOSDK.execute();
 	}
